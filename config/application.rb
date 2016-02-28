1fe5fb71150f3b0560b40e7f91f43ffdac5ed492
Jeff DePree <jdepree@gmail.com>
2016-02-27 19:03:27 -0700

    Precompile images

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Dti
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
+config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif)
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
